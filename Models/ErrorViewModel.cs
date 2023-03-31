b10a9acf0c9ee6004dcf80671bc36d74b0d718c0
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-03-31 18:47:41 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BridgeWater.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
