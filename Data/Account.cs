af62a5ea872da74723e897bf57c476158bfe39a4
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-04-20 21:30:29 +0300

    Add account, order entities

---
+++ Data/Account.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BridgeWater.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Photo { get; set; }
+        [ForeignKey("AccountId")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BridgeWater.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+        public int Stock { get; set; }
+    }
+}
