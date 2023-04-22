8b0c67fa0364f768939bff4f685ffb6039d9961e
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-04-22 15:55:46 +0300

    Add mouse hover on dropdowns

--- wwwroot/js/bridge.js
+++ wwwroot/js/bridge.js
@@ -1,1 +1,19 @@
-﻿
+﻿// Open account menu when mouse is over this control.
+function FadeShow() {
+    let dropdown1 = new bootstrap.Dropdown($('#dropdownMenuLink'));
+    dropdown1.show();
+
+    // hide dropdown
+    let dropdown2 = new bootstrap.Dropdown($('#explore'));
+    dropdown2.hide();
+}
+
+// View store products and plants.
+function ExploreShop() {
+    let dropdown1 = new bootstrap.Dropdown($('#explore'));
+    dropdown1.show();
+
+    // hide account menu
+    let dropdown2 = new bootstrap.Dropdown($('#dropdownMenuLink'));
+    dropdown2.hide();
+}
