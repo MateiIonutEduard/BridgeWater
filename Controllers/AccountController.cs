2a888e17e8498f8911b6f40196d53977f0150c8c
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-04-22 21:11:28 +0300

    Add account page layout

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using BridgeWater.Models;
+using BridgeWater.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BridgeWater.Controllers
+{
+    public class AccountController : Controller
+    {
+        readonly IAccountService accountService;
+
+		public AccountController(IAccountService accountService)
+        { this.accountService = accountService; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signin(AccountRequestModel accountRequestModel)
+        {
+            AccountResponseModel accountResponseModel = await accountService.SignInAsync(accountRequestModel);
+            return View();
+        }
+    }
+}
