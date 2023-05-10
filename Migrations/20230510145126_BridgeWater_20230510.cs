2731bc48c4ed6287e885ceb41ca2931dcc24131d
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-05-10 17:53:04 +0300

    Add webcode field to account table, creates BridgeWater_20230510 migration #7

---
+++ Migrations/20230510145126_BridgeWater_20230510.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BridgeWater.Migrations
+{
+    /// <inheritdoc />
+    public partial class BridgeWater_20230510 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Webcode",
+                table: "Account",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Webcode",
+                table: "Account");
+        }
+    }
+}
