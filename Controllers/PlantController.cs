fd94701701548a7b44e88f32c0713b7f574d389e
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-05-24 20:30:36 +0300

    Fix store plant products filter only by using name #11
    Fix store plant products filter only by using name field.

--- Controllers/PlantController.cs
+++ Controllers/PlantController.cs
@@ -25,12 +25,8 @@ namespace BridgeWater.Controllers
         [HttpPost]
         public async Task<IActionResult> SearchPlants(string? name)
         {
-            string?[] categories = plantService.GetCategories();
-            string index = HttpContext.Request.Query["id"];
-            if (string.IsNullOrEmpty(index)) index = "1";
-
-            string? category = categories[Convert.ToInt32(index) - 1];
-            Plant[] plants = await plantService.GetProductsByNameAsync(name, category);
+            // filter store products only by name, not by using category
+            Plant[] plants = await plantService.GetProductsByNameAsync(name, null);
 
             // route page by case
             if (!string.IsNullOrEmpty(name))
@@ -39,7 +35,7 @@ namespace BridgeWater.Controllers
                 else if (plants.Length == 1) 
                     return Redirect($"/Plant/Details/?id={plants[0].Id}");
                 else
-                    return Redirect("/Plant/");
+                    return Redirect($"/Plant/?name={name}");
             }
             else
                 return Redirect("/Plant/");
