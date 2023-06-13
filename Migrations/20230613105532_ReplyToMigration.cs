662d610a3c1685054b58b0fb2c8cd5ea0c75a8f6
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-13 13:56:44 +0300

    Add post reply migration #13
    Add post reply migration at project.

---
+++ Migrations/20230613105532_ReplyToMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BridgeWater.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplyToMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReplyTo",
+                table: "Post",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReplyTo",
+                table: "Post");
+        }
+    }
+}
