b1c59910bf331211b173b62726111b262d45d572
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-15 14:39:24 +0300

    Add comment with rating object #16
    Add comment with rating object to map post web request fields.

---
+++ Models/CommentRatingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BridgeWater.Models
+{
+    public class CommentRatingModel
+    {
+        public string? message { get; set; }
+        public int? rating { get; set; }
+        public string? plantId { get; set; }
+    }
+}
