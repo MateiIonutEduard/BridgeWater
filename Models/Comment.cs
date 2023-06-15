a6b465b06b1b6c514b9921fef49b600d743cd5f6
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-15 13:12:16 +0300

    Add comment entity for MongoDB #16
    Add comment entity for MongoDB database.

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BridgeWater.Models
+{
+    public class Comment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string? body { get; set; }
+        public int? rating { get; set; }
+        public DateTime createdAt { get; set; }
+
+        public bool? isDeleted { get; set; }
+        public string? replyTo { get; set; }
+        public int accountId { get; set; }
+    }
+}
