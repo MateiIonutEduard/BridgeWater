400ec6fcec6cdd342760027b2277d3831d65fca5
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2024-05-02 12:44:56 +0300

    Add payment gateway system #29
    Add payment gateway service.

---
+++ Services/IBraintreeService.cs
@@ -0,0 +1,11 @@
+﻿using Braintree;
+
+namespace BridgeWater.Services
+{
+    /* Braintree interface used to do implement virtual payments. */
+    public interface IBraintreeService
+    {
+        IBraintreeGateway CreateGateway();
+        IBraintreeGateway GetGateway();
+    }
+}
